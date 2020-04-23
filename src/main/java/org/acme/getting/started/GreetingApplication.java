39e99f2de79252d4d584625e4b567ff53b1581a1
dmi3coder <dmi3coder@gmail.com>
2020-04-23 23:19:02 +0300

    Fixed formatting

--- src/main/java/org/acme/getting/started/GreetingApplication.java
+++ src/main/java/org/acme/getting/started/GreetingApplication.java
@@ -1,9 +1,9 @@
 package org.acme.getting.started;
 
-import javax.inject.Inject;
-import org.jboss.logging.Logger;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
+import org.jboss.logging.Logger;
+import javax.inject.Inject;
 
 @QuarkusMain
 public class GreetingApplication implements QuarkusApplication {
@@ -11,23 +11,22 @@ public class GreetingApplication implements QuarkusApplication {
 
     @Inject
     GreetingService greetingService;
-    
-	@Override
-	public int run(String... args) throws Exception {
-        if(args[0].equals("--help")) {
+
+    @Override
+    public int run(String... args) throws Exception {
+        if (args[0].equals("--help")) {
             LOGGER.info(
-                "\nThis tool helps greet users by their name\n" +
-                "Available commands:\n" +
-                "\t--help\t\t\tShow helpful message\n" +
-                "\t--greet={name}\t\tGreet person by their name"
+                    "\nThis tool helps greet users by their name\n" +
+                    "Available commands:\n" +
+                    "\t--help\t\t\tShow helpful message\n" +
+                    "\t--greet={name}\t\tGreet person by their name"
             );
             return 0;
         }
-        if(args[0].startsWith("--greet")) { 
+        if (args[0].startsWith("--greet")) {
             String name = args[0].substring(8);
             LOGGER.info(greetingService.greeting(name));
-            return 0;
-        }
-		return 0;
-	}
+        } // ... more else if cases for diferent commands
+        return 0;
+    }
 }
