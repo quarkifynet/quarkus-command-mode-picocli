bbb5cc9db33908b0bad842ced69be10c0fc1d5d1
dmi3coder <dmi3coder@gmail.com>
2020-04-26 16:58:10 +0300

    Some formatting enhancements

--- src/main/java/org/acme/getting/started/command/SetCommand.java
+++ src/main/java/org/acme/getting/started/command/SetCommand.java
@@ -2,24 +2,20 @@ package org.acme.getting.started.command;
 
 import org.acme.getting.started.data.Setting;
 import picocli.CommandLine;
-
 import javax.enterprise.context.Dependent;
 import javax.transaction.Transactional;
 
 @Dependent
 @CommandLine.Command(name = "set")
 public class SetCommand implements Runnable {
-    @CommandLine.Parameters(index = "0")
-    TableType type;
-    @CommandLine.Parameters(index = "1")
-    String key;
-    @CommandLine.Parameters(index = "2")
-    String value;
+    @CommandLine.Parameters(index = "0") TableType type;
+    @CommandLine.Parameters(index = "1") String key;
+    @CommandLine.Parameters(index = "2") String value;
 
     @Override
     @Transactional
     public void run() {
-        if (type == TableType.SETTING) { //make a switch in future
+        if (type == TableType.SETTING) { //make a switch when you have more options
             Setting setting = Setting.find("key = ?1", key).firstResult();
             if(setting == null) {
                 setting = new Setting();
@@ -30,7 +26,5 @@ public class SetCommand implements Runnable {
         }
     }
 
-    enum TableType {
-        SETTING
-    }
+    enum TableType { SETTING }
 }
--- src/main/java/org/acme/getting/started/data/Setting.java
+++ src/main/java/org/acme/getting/started/data/Setting.java
@@ -1,12 +1,7 @@
 package org.acme.getting.started.data;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Setting extends PanacheEntityBase {
